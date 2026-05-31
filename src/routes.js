27808087e6e56aff6c30ee65bf4041e6dbfc5c56
Momade Ibraimo <momadeibraimoh@gmail.com>
2026-05-31 21:08:21 +0200

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Containers/Home";
 import UsersOrder from "./Containers/UsersOrder";
 
-
-
 function MyRoutes() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/orders" element={<UsersOrder />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
